b35b43b082f48303b9cd5a1dae2aed94adb35ad3
Christine Kommu <ckommu@ggc.edu>
2024-02-25 10:29:51 -0500

    separated stack and bottom tab navigation, added bottom tab navbar

--- App.js
+++ App.js
@@ -1,64 +1,16 @@
-import * as React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import Login from "./app/screens/Login";
-import Listing from "./app/screens/Listing";
-import SignUp from "./app/screens/SignUp";
-import Profile from "./app/screens/Profile";
-
-
-const Stack = createNativeStackNavigator();
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AppNavigation from "./app/navigation/AppNavigation";
 
 export default function App() {
-  
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-          name="Login" 
-          component={Login} 
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen 
-          name="SignUp" 
-          component={SignUp} 
-          options={{
-            headerTitle: '',
-          }}
-        />
-        <Stack.Screen 
-          name="Profile" 
-          component={Profile} 
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen 
-          name="Listing" 
-          component={Listing} 
-        />
-      
-      </Stack.Navigator>
-    </NavigationContainer>
-    
-    //<View style={styles.container}>
-      //<Text>Open up App.js to start working on your app!!!</Text>
-      //<StatusBar style="auto" />
-    //</View>
-  );
+  return <AppNavigation />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ app/navigation/AppNavigation.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+
+import Login from "../screens/Stacks/Login";
+import SignUp from "../screens/Stacks/SignUp";
+import Home from "../screens/Tabs/Home";
+import Profile from "../screens/Tabs/Profile";
+import Wishlist from "../screens/Tabs/Wishlist";
+import Cart from "../screens/Tabs/Cart";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+export default function AppNavigation() {
+  function MyStack() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Tabs"
+          component={MyTabs}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    );
+  }
+
+  function MyTabs() {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Wishlist" component={Wishlist} />
+        <Tab.Screen name="Cart" component={Cart} />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
+    );
+  }
+
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
