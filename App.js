07f8b999b9a12185b181e213a13a2ae201f2df91
cynthiakem <cynthia.kem449@gmail.com>
2024-02-25 11:04:14 -0500

    added splash screens

--- App.js
+++ App.js
@@ -8,9 +8,69 @@ import Login from "./app/screens/Login";
 import Listing from "./app/screens/Listing";
 import SignUp from "./app/screens/SignUp";
 import Profile from "./app/screens/Profile";
-
+import OnboardingScreen from './app/screens/OnboardingScreen';
+import { AsyncStorage } from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator();
+
+// shows splash screen on first lauch only
+const lauch = () => {
+  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
+  React.useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+      if(value = null) {
+        AsyncStorage.setItem('alreadyLaunched', 'true');
+        setIsFirstLaunch(true);
+      } else {
+        setIsFirstLaunch(false);
+      }
+    });
+  }, []);
+
+  if( isFirstLaunch == null ) {
+    return null;
+  } else if (isFirstLaunch == true){
+    return(
+      <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen 
+          name="OnboardingScreen" 
+          component={OnboardingScreen} 
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen 
+          name="Login" 
+          component={Login} 
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen 
+          name="SignUp" 
+          component={SignUp} 
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen 
+          name="Profile" 
+          component={Profile} 
+        />
+        <Stack.Screen 
+          name="Listing" 
+          component={Listing} 
+        />
+      
+      </Stack.Navigator>
+    </NavigationContainer>
+    );
+  } else {
+          <Login />
+  }
+
+}
 
 export default function App() {
   
@@ -18,6 +78,13 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+      <Stack.Screen 
+          name="OnboardingScreen" 
+          component={OnboardingScreen} 
+          options={{
+            headerShown: false,
+          }}
+        />
         <Stack.Screen 
           name="Login" 
           component={Login} 
---
+++ app/screens/OnboardingScreen.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
+import Onboarding from 'react-native-onboarding-swiper';
+
+const OnboardingScreen = ({navigation}) => {
+    
+    return (
+        <Onboarding
+        onSkip={() => navigation.replace("Login")}
+        onDone={() => navigation.navigate("Login")}
+        pages={[
+            {
+              backgroundColor: '#A2CBF8',
+              image: <Image source={require('../assets/trade.png')} />,
+              title: 'Swap & Sell',
+              subtitle: 'Trade, sell, and buy from students on campus ',
+            },
+            {
+                backgroundColor: '#FF9494',
+                image: <Image source={require('../assets/clock.png')} />,
+                title: 'Schedule Meetups',
+                subtitle: 'Select dates, times, and locations\nthat work for you',
+              },
+              {
+                backgroundColor: '#FFBD59',
+                image: <Image source={require('../assets/check.png')} />,
+                title: 'Safe on Campus',
+                subtitle: 'Meet at well known locations across campus',
+              }
+          ]}
+      />
+    );
+};
+
+export default OnboardingScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+});
